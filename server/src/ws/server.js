fde67f30402d6412df04637b6239ee6b165c0101
kaushikkanduri <kaushikkanduri883@gmail.com>
2026-04-04 23:02:02 +0530

    feat: implement heartbeat mechanism to terminate dead connections

--- server/src/ws/server.js
+++ server/src/ws/server.js
@@ -14,14 +14,27 @@ function broadcast(wss, payload){
 export function createWebSocketServer(server){
     //1MB - 1024*1024
     const wss = new WebSocketServer({server, path: "/ws", maxPayload: 1024*1024 });
-    
     wss.on("connection", function(socket){
-        console.log("New user connected");
+        socket.isAlive = true;
         sendJson(socket, {type: "welcome", payload: {data: "Connection established" }});
 
         socket.on("error", (err) => console.error(err));
         socket.on("close", () => console.log("User disconnected"));
+
+        socket.on("pong", () => socket.isAlive = true);
     })
+
+    let interval = setInterval(()=>{
+
+        for(const client of wss.clients){
+            if(client.isAlive === false) return client.terminate();
+            client.isAlive = false;
+            client.ping();
+        }
+
+    }, 30*1000);
+
+    wss.on("close", ()=> clearInterval(interval));
 
     function broadcastMatchCreated(match){
         broadcast(wss, {type: "match_created", payload: {data: match}})
